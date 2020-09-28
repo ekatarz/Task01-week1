b80772578c97eec579a9f102869f1ae02d5c1d14
ekatarzyna <ekate.uk@gmail.com>
2020-09-28 17:27:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+starter=input("What is your favourite starter? ")
+main=input("What is your favourite main course? ")
+dessert=input("What is your favourite dessert? ")
+drink=input("What is your favourite drink? ")
+
+print ("Your favourite meal is ", starter, ",", main, "and", dessert, "with a glass of", drink, "!")
+
+
+
